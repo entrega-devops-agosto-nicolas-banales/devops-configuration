1ddba41375bce171c3d4413b903658b59115a4b6
Nico B Trivelli <ninotrivelli@gmail.com>
2024-11-25 02:07:10 -0300

    fixed main flow

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
   region         = var.aws_region
   access_key     = var.aws_access_key
   secret_key     = var.aws_secret_key
-  session_token  = var.aws_session_token
 }
 
 data "aws_vpc" "default" {
