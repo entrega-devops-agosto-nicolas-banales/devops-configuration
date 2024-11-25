266eea691325d1e141b61e5b337fcfcaf32c10f5
Nico B Trivelli <ninotrivelli@gmail.com>
2024-11-25 19:34:01 -0300

    deprecated line in main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "public" {
+data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
