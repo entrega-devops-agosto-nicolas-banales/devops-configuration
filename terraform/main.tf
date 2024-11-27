0651c0ee90d1055edc81e6729fd101a1aebfa9e1
Nico B Trivelli <ninotrivelli@gmail.com>
2024-11-26 22:51:52 -0300

    updated dockerfile of oderds and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,27 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Permitir acceso de cualquier lado
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   egress {
@@ -46,8 +66,8 @@ resource "aws_ecs_task_definition" "task_definition" {
   family                   = each.key
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256" # Ajusta según necesidades
-  memory                   = "512" # Ajusta según necesidades
+  cpu                      = "256"
+  memory                   = "512"
 
   container_definitions = jsonencode([
     {
@@ -60,15 +80,30 @@ resource "aws_ecs_task_definition" "task_definition" {
           protocol      = "tcp"
         }
       ]
+      environment = each.key == "orders-service" ? [
+        {
+          name  = "PAYMENTS_SERVICE_ENDPOINT"
+          value = var.payments_service_endpoint
+        },
+        {
+          name  = "PRODUCTS_SERVICE_ENDPOINT"
+          value = var.products_service_endpoint
+        },
+        {
+          name  = "SHIPPING_SERVICE_ENDPOINT"
+          value = var.shipping_service_endpoint
+        }
+      ] : null
     }
   ])
 }
+
 
 resource "aws_lb" "application_lbs" {
   for_each            = toset(var.service_names)
   name                = "${each.key}-alb"
   load_balancer_type  = "application"
-  security_groups     = [aws_security_group.ecs_sg.id]
+  security_groups     = [aws_security_group.alb_sg.id]
   subnets             = data.aws_subnets.public.ids
   enable_deletion_protection = false
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,3 +38,16 @@ variable "service_health_paths" {
     "shipping-service" = "/"
   }
 }
+
+variable "payments_service_endpoint" {
+  type = string
+}
+
+variable "products_service_endpoint" {
+  type = string
+}
+
+variable "shipping_service_endpoint" {
+  type = string
+}
+
