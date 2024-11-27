ab4dd24fac00cb944415700f67369d34a348146e
Nico B Trivelli <ninotrivelli@gmail.com>
2024-11-26 23:21:08 -0300

    updated maintf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,8 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+    description     = "Permite acceso desde las tareas ECS"
     cidr_blocks = ["0.0.0.0/0"] 
   }
 
